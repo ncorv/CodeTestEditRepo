8c9ae36f2248654c884e78fe59878cfd859e5d27
ncorv <nick@piss.tech>
2024-02-28 02:31:45 -0500

    Update file: repo\directory\example3.py

---
+++ directory\example3.py
@@ -0,0 +1,11 @@
+def main():
+    user_input = input("Enter a command: ")
+    if user_input.strip() == "Send Spike":
+        print("Spike Sent")
+    else:
+        print("Unknown command")
+
+if __name__ == "__main__":
+    main()
+
+# asdklfjasdlk;fasjdfl;kasdklfj
