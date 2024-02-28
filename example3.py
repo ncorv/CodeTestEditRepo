1630e50a2c3119590d0803aa2b3cabc4e134f9f7
ncorv <nick@piss.tech>
2024-02-28 03:54:21 -0500

    Committing repo\directory\example3.py

---
+++ example3.py
@@ -0,0 +1,13 @@
+def main():
+    user_input = input("Enter a command: ")
+    if user_input.strip() == "Send Spike":
+        print("Spike Sent")
+    else:
+        print("Unknown command")
+
+if __name__ == "__main__":
+    main()
+
+# i made an edit and saved it
+
+# look at this another edit
