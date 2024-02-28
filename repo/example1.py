69933a5901049a4d3dd019b854bf0c6ae9d1f716
ncorv <nick@piss.tech>
2024-02-28 03:51:00 -0500

    Committing repo\example1.py

---
+++ repo/example1.py
@@ -0,0 +1,16 @@
+import time
+from datetime import datetime
+
+def print_date_every_second():
+    try:
+        while True:
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            print(current_time)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Program terminated by user.")
+
+
+print_date_every_second()
+
+# asdf 1234 asdf 1234
