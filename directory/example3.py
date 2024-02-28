0303a39906b10e54847ccfd3ad8d0a33a0ae7017
ncorv <nick@piss.tech>
2024-02-28 02:00:04 -0500

    Create example3.py

---
+++ directory/example3.py
@@ -0,0 +1,9 @@
+def main():
+    user_input = input("Enter a command: ")
+    if user_input.strip() == "Send Spike":
+        print("Spike Sent")
+    else:
+        print("Unknown command")
+
+if __name__ == "__main__":
+    main()
